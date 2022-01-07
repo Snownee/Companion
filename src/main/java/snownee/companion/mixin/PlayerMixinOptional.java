a54a0961715a93bc8ebed1e8ea1f8c77e0c1e8c5
Snownee <mjy523@126.com>
2022-01-07 17:11:38 +0800

    fix: #1

---
+++ src/main/java/snownee/companion/mixin/PlayerMixinOptional.java
@@ -0,0 +1,22 @@
+package snownee.companion.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.world.entity.player.Player;
+import snownee.companion.CompanionPlayer;
+
+@Mixin(value = Player.class, priority = 950)
+public abstract class PlayerMixinOptional implements CompanionPlayer {
+
+	@Redirect(
+			at = @At(
+					value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;removeEntitiesOnShoulder()V"
+			), method = { "aiStep", "hurt" }, require = 0
+	)
+	private void nullifyDefaultRemoveEntitiesOnShoulder(Player player) {
+		// NOOP
+	}
+
+}
