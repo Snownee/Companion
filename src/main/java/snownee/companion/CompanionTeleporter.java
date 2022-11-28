b6195c8c85503c3acc0608a57c026a0484217a02
Snownee <i@snownee.com>
2022-11-28 18:58:24 +0800

    improve: attempt to teleport pets in more conditions

---
+++ src/main/java/snownee/companion/CompanionTeleporter.java
@@ -0,0 +1,33 @@
+package snownee.companion;
+
+import java.util.function.Function;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.portal.PortalInfo;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.util.ITeleporter;
+
+public class CompanionTeleporter implements ITeleporter {
+
+	public static final CompanionTeleporter INSTANCE = new CompanionTeleporter();
+
+	@Override
+	public @Nullable PortalInfo getPortalInfo(Entity entity, ServerLevel destWorld, Function<ServerLevel, PortalInfo> defaultPortalInfo) {
+		Player owner = Hooks.getEntityOwner(entity);
+		if (owner == null) {
+			return null;
+		}
+		Vec3 dest = Hooks.teleportWithRandomOffset(owner, owner.blockPosition()).orElseGet(owner::position);
+		return new PortalInfo(dest, Vec3.ZERO, entity.getYRot(), entity.getXRot());
+	}
+
+	@Override
+	public boolean playTeleportSound(ServerPlayer player, ServerLevel sourceWorld, ServerLevel destWorld) {
+		return false;
+	}
+}
