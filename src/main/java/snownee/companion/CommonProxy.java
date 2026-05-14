e3e7687de8d01b9ebd321ca8d0160ce2aec41719
SettingDust <settingdust@gmail.com>
2026-05-14 20:45:44 +0800

    fix: register gamerules with NeoForge event

--- src/main/java/snownee/companion/CommonProxy.java
+++ src/main/java/snownee/companion/CommonProxy.java
@@ -2,19 +2,42 @@ package snownee.companion;
 
 import java.util.Objects;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.gamerules.GameRuleCategory;
+import net.minecraft.world.level.gamerules.GameRules;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.Tags;
+import net.neoforged.neoforge.registries.RegisterEvent;
 
 @Mod(Companion.ID)
 public class CommonProxy {
 	public CommonProxy(IEventBus modBus) {
-		Objects.requireNonNull(Companion.PET_FRIENDLY_FIRE);
+		Objects.requireNonNull(Companion.IMMORTAL_BLACKLIST);
 		modBus.addListener(CompanionDataGen::gatherData);
 	}
 
 	public static boolean isRangedWeapon(ItemStack itemStack) {
 		return itemStack.is(Tags.Items.RANGED_WEAPON_TOOLS);
+	}
+
+	@SubscribeEvent
+	static void registerSetup(RegisterEvent event) {
+		if (event.getRegistry().equals(BuiltInRegistries.GAME_RULE)) {
+			Companion.PET_FRIENDLY_FIRE = GameRules.registerBoolean(
+					Companion.ID + ":pet_friendly_fire",
+					GameRuleCategory.PLAYER,
+					true);
+			Companion.IMMORTAL_PETS = GameRules.registerBoolean(
+					Companion.ID + ":immortal_pets",
+					GameRuleCategory.PLAYER,
+					false);
+			Companion.ALWAYS_TELEPORT_HORSES = GameRules.registerBoolean(
+					Companion.ID + ":always_teleport_horses",
+					GameRuleCategory.PLAYER,
+					false);
+		}
 	}
 }
--- src/main/java/snownee/companion/Companion.java
+++ src/main/java/snownee/companion/Companion.java
@@ -1,5 +1,6 @@
 package snownee.companion;
 
+import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -10,25 +11,14 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.gamerules.GameRule;
-import net.minecraft.world.level.gamerules.GameRuleCategory;
-import net.minecraft.world.level.gamerules.GameRules;
 
 public class Companion {
 	public static final String ID = "companion";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public static final GameRule<Boolean> PET_FRIENDLY_FIRE = GameRules.registerBoolean(
-			Companion.ID + ":pet_friendly_fire",
-			GameRuleCategory.PLAYER,
-			true);
-	public static final GameRule<Boolean> IMMORTAL_PETS = GameRules.registerBoolean(
-			Companion.ID + ":immortal_pets",
-			GameRuleCategory.PLAYER,
-			false);
-	public static final GameRule<Boolean> ALWAYS_TELEPORT_HORSES = GameRules.registerBoolean(
-			Companion.ID + ":always_teleport_horses",
-			GameRuleCategory.PLAYER,
-			false);
+	public static @Nullable GameRule<Boolean> PET_FRIENDLY_FIRE;
+	public static @Nullable GameRule<Boolean> IMMORTAL_PETS;
+	public static @Nullable GameRule<Boolean> ALWAYS_TELEPORT_HORSES;
 
 	public static final TagKey<EntityType<?>> IMMORTAL_BLACKLIST = TagKey.create(Registries.ENTITY_TYPE, id("immortal_blacklist"));
 	public static final TagKey<Item> CHARGED_RANGED_WEAPONS = TagKey.create(Registries.ITEM, id("charged_ranged_weapons"));
