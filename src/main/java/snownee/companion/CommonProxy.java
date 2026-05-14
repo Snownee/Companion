3fe8a78f29a817d16fff4db4e92f0bde07b3023a
SettingDust <settingdust@gmail.com>
2026-05-14 20:34:13 +0800

    feat: port datagen to neoforge

--- src/main/java/snownee/companion/CommonProxy.java
+++ src/main/java/snownee/companion/CommonProxy.java
@@ -3,13 +3,15 @@ package snownee.companion;
 import java.util.Objects;
 
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.Tags;
 
 @Mod(Companion.ID)
 public class CommonProxy {
-	public CommonProxy() {
+	public CommonProxy(IEventBus modBus) {
 		Objects.requireNonNull(Companion.PET_FRIENDLY_FIRE);
+		modBus.addListener(CompanionDataGen::gatherData);
 	}
 
 	public static boolean isRangedWeapon(ItemStack itemStack) {
