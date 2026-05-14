f0977822b01fd01ebd72f5add8e868dbe70c2316
SettingDust <settingdust@gmail.com>
2026-05-14 21:45:26 +0800

    chore: register the listener

--- src/main/java/snownee/companion/CommonProxy.java
+++ src/main/java/snownee/companion/CommonProxy.java
@@ -7,7 +7,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.gamerules.GameRuleCategory;
 import net.minecraft.world.level.gamerules.GameRules;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.registries.RegisterEvent;
@@ -17,13 +16,13 @@ public class CommonProxy {
 	public CommonProxy(IEventBus modBus) {
 		Objects.requireNonNull(Companion.IMMORTAL_BLACKLIST);
 		modBus.addListener(CompanionDataGen::gatherData);
+		modBus.addListener(CommonProxy::registerSetup);
 	}
 
 	public static boolean isRangedWeapon(ItemStack itemStack) {
 		return itemStack.is(Tags.Items.RANGED_WEAPON_TOOLS);
 	}
 
-	@SubscribeEvent
 	static void registerSetup(RegisterEvent event) {
 		if (event.getRegistry().equals(BuiltInRegistries.GAME_RULE)) {
 			Companion.PET_FRIENDLY_FIRE = GameRules.registerBoolean(
